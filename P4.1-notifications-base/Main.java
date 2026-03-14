25599917a22f5229d5e97fd82738ac46f3be0944
Jackie102 <pawel.sza@educa.jcyl.es>
2026-03-15 00:02:02 +0100

    test: añadir pruebas para detectar libro duplicado

--- P4.1-notifications-base/Main.java
+++ P4.1-notifications-base/Main.java
@@ -4,13 +4,8 @@ public class Main {
         NotificationManager manager = new NotificationManager();
         
         // Ejemplos de uso
-<<<<<<< HEAD
         manager.send("email", "miau al sistema", "usuario@email.com");
         manager.send("sms", "Tu código es 3321", "+34123456789");
-=======
-        manager.send("email", "tung tung sahur al sistema", "usuario@email.com");
-        manager.send("sms", "Tu código es 1234", "+34123456789");
->>>>>>> 9260957ffb6ca09eb4fc0e160029864cad5e05bf
         manager.send("push", "Tienes un nuevo mensaje", "user_device_001");
     }
 }
