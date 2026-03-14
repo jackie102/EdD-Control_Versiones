d9d5b84ffb308c61eeb3aa2304ebc8a56654ec49
jackie102 <124812840+jackie102@users.noreply.github.com>
2026-03-14 12:37:48 +0100

    se cambio el ejemplo de codigo

--- P4.1-notifications-base/Main.java
+++ P4.1-notifications-base/Main.java
@@ -5,7 +5,7 @@ public class Main {
         
         // Ejemplos de uso
         manager.send("email", "Bienvenido al sistema", "usuario@email.com");
-        manager.send("sms", "Tu código es 1234", "+34123456789");
+        manager.send("sms", "Tu código es 3321", "+34123456789");
         manager.send("push", "Tienes un nuevo mensaje", "user_device_001");
     }
 }
